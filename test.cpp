578bbdc56b2568dd3689a34ffb3f661e021946b3
fliptrail <amishmittal@outlook.com>
2021-06-04 23:17:52 +0530

    initialize templating of vector

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include "vector.h"
 #include <iostream>
+// #include <bits/stdc++.h>
 
 using namespace std;
 using namespace custom_stl;
 
 int main(){
-    vector a;
+    vector<double> a;
     return 0;
 }
--- vector.h
+++ vector.h
@@ -1,8 +1,14 @@
+#include <iostream>
+
 // vector <T> class
 namespace custom_stl{
-    class vector{
-        int a;
+    template <typename T> class vector{
+        T a;
         public:
             vector();
     };
+
+    template <typename T> vector<T>::vector(){
+        std::cout<<"Vector initialized"<<std::endl;
+    }
 }
