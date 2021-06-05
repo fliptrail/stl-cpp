142f02aa4e6635d2fa6a81b47a46cd498523777a
fliptrail <amishmittal@outlook.com>
2021-06-05 17:18:39 +0530

    initialize algorithms.h

---
+++ algorithms.h
@@ -0,0 +1,10 @@
+// Declares common STL algorithms for any container
+#include <iostream>
+
+namespace custom_stl{
+    template <typename T> void printall(T * begin, T * end, char sep = ' ');
+
+    template <typename T> void printall(T * begin, T * end, char sep = ' '){
+        ;
+    }
+}
