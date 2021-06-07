e85ddc5366cac3d40a0fa5450ca33517beeabf63
fliptrail <amishmittal@outlook.com>
2021-06-08 02:31:35 +0530

    derive set from bst class

---
+++ bst.h
@@ -0,0 +1,6 @@
+// Binary Search Tree class to be inherited by other STL containers
+namespace custom_stl{
+    template <typename T> class bst{
+        
+    };
+}
--- set.h
+++ set.h
@@ -1,6 +1,8 @@
 // set<T> class
+#include "bst.h"
+
 namespace custom_stl{
-    template <typename T> class set{
+    template <typename T> class set : public custom_stl::bst<T>{
 
     };
 }
